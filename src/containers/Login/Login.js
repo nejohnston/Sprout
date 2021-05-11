256d51b9275616a4fc22c096e1c9a0a9af5c66b9
Susan Li <susansemail195@gmail.com>
2021-05-10 22:43:45 -0700

    Build static login page with form control.

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Login.css";
 
 const Login = () => {
-
   return (
-  <div>
-    <p>
-      This is where components will reside
-    </p>
-  </div>);
+    <>
+      <div id="bg">
+        <div id="login-card">
+          <Form>
+            <h3>Login or Signup</h3>
+            <hr />
+            <Form.Group controlId="formUsername">
+              <Form.Label>Username</Form.Label>
+              <Form.Control type="text" placeholder="Enter Username" />
+            </Form.Group>
+
+            <Form.Group controlId="formPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Enter Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Login or Signup
+            </Button>
+          </Form>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Login;
