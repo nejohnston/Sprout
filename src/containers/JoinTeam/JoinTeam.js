14e7cba0ce301c5c45e45ae52794aa54dadab401
Susan Li <susansemail195@gmail.com>
2021-05-10 22:59:48 -0700

    Made folder structure for join team screen and components.

---
+++ src/containers/JoinTeam/JoinTeam.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const JoinTeam = () => {
+  return (
+  <div>
+    <p>
+      This is where components will reside
+    </p>
+  </div>);
+};
+
+export default JoinTeam;
