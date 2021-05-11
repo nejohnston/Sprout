2951e81fa783c625c5a4dac37e85a386c7e9cf07
Sally Poon <sallypoon@hotmail.com>
2021-05-11 13:12:12 -0700

    EDIT minor typos

--- src/containers/About-Us/About-Us.js
+++ src/containers/About-Us/About-Us.js
@@ -11,17 +11,17 @@ const AboutUs = () => {
   const classes = useStyles();
   return (
   <>
-    <h1>'Growing' the knowledge for plant life</h1>
+    <h1>Nurturing the knowledge for plant life</h1>
 
     <div>
     <h2>Our Story</h2>
     <p>Banded together by an interest to support sustainable living and gardening,
-       our team came up with <em>Sprout</em>, our gardening application to promote
+       our team came up with <em>Sprout</em>, a gardening application to promote
        bee-friendly and native gardens across the Greater Vancouver area.
         <br/><br/>
        With this application, we hope that users can connect with the community while
        learning more about the ecosystem around them. We welcome users of all ages
-       and expertise to join us in 'growing' our knowledge about plant-care and plant-life!
+       and expertise to join us in nurturing their knowledge about plant-care and plant-life!
     </p>
     </div>
 
