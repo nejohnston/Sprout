d06c55fb4adf8ffbb57f8ea058c1d563a97b94b1
Susan Li <susansemail195@gmail.com>
2021-05-12 12:32:06 -0700

    Push work so far so Sally can also start working on the components.

--- src/containers/Profile/SproutGallery.js
+++ src/containers/Profile/SproutGallery.js
@@ -6,7 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const SproutGallery = ({sprouts}) => {
   return (
   <>
-    <SproutItem sprouts= {sprouts}/>
+    <SproutItem sprouts={sprouts}/>
   </>);
 };
 
--- src/containers/Profile/SproutItem.js
+++ src/containers/Profile/SproutItem.js
@@ -6,7 +6,7 @@ const PlantGallery = ({sprouts}) => {
   return sprouts.map((sprout) => (
     <div className="plant-list-item" key={sprout.id}>
       <img src={sprout.image_url} className="plant-list-img"/>
-      <p className="plant-list-name">{sprout.name}</p>
+      <strong><p className="plant-list-name">{sprout.name}</p></strong>
     </div>
   ));
 };
