5f419835cefc3ff2d6adc81f2153f30adebe1ab2
Nicholas Johnston <nicholas.johnston94@gmail.com>
2021-05-12 14:48:41 -0700

    added new client/server file structure and express server

--- server/index.js
+++ server/index.js
@@ -2,10 +2,45 @@ const express = require('express')
 const app = express()
 const port = 3001
 
+const user_model = require('./config/pgConnect')
+
+app.use(express.json())
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+  next();
+});
+
 app.get('/', (req, res) => {
-  res.status(200).send('Hello World!');
+  user_model.getUser()
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
 })
 
+// app.post('/merchants', (req, res) => {
+//   user_model.createMerchant(req.body)
+//   .then(response => {
+//     res.status(200).send(response);
+//   })
+//   .catch(error => {
+//     res.status(500).send(error);
+//   })
+// })
+
+// app.delete('/merchants/:id', (req, res) => {
+//   user_model.deleteMerchant(req.params.id)
+//   .then(response => {
+//     res.status(200).send(response);
+//   })
+//   .catch(error => {
+//     res.status(500).send(error);
+//   })
+// })
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
 })
