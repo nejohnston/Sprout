06258b34297a0e4f49625856e48190841c28850d
Susan Li <susansemail195@gmail.com>
2021-05-12 14:38:11 -0700

    Add profile header.

--- src/containers/Profile/Profile.js
+++ src/containers/Profile/Profile.js
@@ -3,16 +3,24 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import SproutGallery from './SproutGallery';
 import ProfilePicture from './ProfilePicture';
 import userdata from "./user.json";
+import addButton from './images/addbutton.svg'
+import './styles/Profile.css'
 
 const Profile = () => {
   let userjson = []
   userjson.push(...userdata);
 
   return (
-  <>
+  <div id="container">
+    <div id="profile-header">
+      <h1 id="profile-h1">My Sprouts</h1>
+      <img src={addButton} id="profile-add-button"/>
+    </div>
+    <hr />
     <ProfilePicture profilePic={userjson[0]["profile_pic"]}/>
     <SproutGallery sprouts={userjson[0]["sprouts"]}/>
-  </>);
+    <div id="vector-bg"></div>
+  </div>);
 };
 
 export default Profile;
