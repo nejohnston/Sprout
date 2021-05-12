83da2bdd533e46fb20ab2fc36bc29fe2309eaa5d
Nicholas Johnston <nicholas.johnston94@gmail.com>
2021-05-12 09:56:27 -0700

    updated file structure to client and server sections

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3001
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello World!');
+})
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
