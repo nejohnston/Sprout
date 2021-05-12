9e46475a7cb5a7af3468bae0c39040b737b143f3
Nicholas Johnston <nicholas.johnston94@gmail.com>
2021-05-12 07:50:42 -0700

    removed useEffect from leaderboard

--- src/containers/Leaderboards/LeaderboardContainer.js
+++ src/containers/Leaderboards/LeaderboardContainer.js
@@ -2,10 +2,6 @@ import React, { useEffect } from 'react';
 import Leaderboard from './Leaderboard'
 
 const LeaderboardContainer = () => {
-  useEffect(() => {
-    getUsers();
-    });
-
   return <Leaderboard />
 }
 
