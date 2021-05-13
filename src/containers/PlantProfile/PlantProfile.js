4b6aabc199b039c2d1965aaa21d5938e679b31da
Susan Li <susansemail195@gmail.com>
2021-05-13 15:03:18 -0700

    Finish header.

--- src/containers/PlantProfile/PlantProfile.js
+++ src/containers/PlantProfile/PlantProfile.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import plantprofiledata from './plant_profile.json'
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/PlantProfile.css";
+import plantprofiledata from "./plant_profile.json";
+import BackButton from "./images/back_button.svg";
+import EditButton from "./images/pen.svg";
+import { Link } from "react-router-dom";
 
 const PlantProfile = () => {
-
-  let plantprofilejson = []
+  let plantprofilejson = [];
   plantprofilejson.push(...plantprofiledata);
-  
+
   return (
-  <div>
-    <p>
-      This is where Profile components will reside
-    </p>
-  </div>
+    <div id="container">
+      <div className="header_backarrow_container">
+        <Link to="/profile">
+          <img src={BackButton} className="back-button" />
+        </Link>
+        <h1 id="plant-profile-h1">{plantprofilejson[0]["user_given_name"]}</h1>
+        <img src={EditButton} id="edit-button" />
+      </div>
+      <hr />
+    </div>
   );
 };
 
