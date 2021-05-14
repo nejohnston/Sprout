4cf3244d4fbb334a831609e25c4e03463ba22add
Susan Li <susansemail195@gmail.com>
2021-05-13 23:07:51 -0700

    Rewrote funciton with ternary operator in Layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,18 +3,13 @@ import PropTypes from "prop-types";
 import NavBar from "./NavBar";
 
 export const Layout = ({ children }) => {
-  const displayNav = () => {
-    if (
-      window.location.pathname != "/login" ||
-      window.location.pathname != "/join-team"
-    ) {
-      <NavBar />;
-    }
-  };
   return (
     <div>
       <div className="appContent">{children}</div>
-      {displayNav}
+      {window.location.pathname != "/login" &&
+      window.location.pathname != "/join-team" ? (
+        <NavBar />
+      ) : null}
     </div>
   );
 };
