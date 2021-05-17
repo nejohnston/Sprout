8e15d2281b99a29aaab42d6d05b6b3d2601d80a5
Susan Li <susansemail195@gmail.com>
2021-05-17 15:33:10 -0700

    Initial folder structure and JSON file

---
+++ src/containers/SearchPlantDetail/SearchPlantDetailContainer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import SearchPlantDetail from './SearchPlantDetail'
+
+const SearchPlantDetailContainer = () => {
+  return <SearchPlantDetail />;
+}
+
+export default SearchPlantDetailContainer;
