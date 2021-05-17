c66136d2ff93b6603ec72533ac8e0d49a9588ee7
Susan Li <susansemail195@gmail.com>
2021-05-17 12:37:41 -0700

    Implement function to filter out non native plants

--- src/containers/Search/Search.js
+++ src/containers/Search/Search.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import {React, useEffect} from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import searchPlantData from "./plants.json";
+import SearchItem from "./SearchItems";
 
 const Search = () => {
+  let plantJSON = [];
+  plantJSON.push(...searchPlantData);
+  useEffect(() => {
+    let nativePlants = document.getElementsByClassName("is-native");
+    console.log(nativePlants.length)
+    console.log(document.getElementById("plant1").innerHTML);
+    for (let i = 1; i < nativePlants.length + 1; i++) {
+      if(plantJSON[i-1]["PLANT_ORIGIN"].substring(0, 4) !== "B.C.") {
+        let is_native = document.getElementById("plant" + String(i));
+        is_native.remove()
+      }
+    } 
+  });
+
   return (
-  <div>
-    <p>Hello World</p>
-  </div>);
+    <>
+      <SearchItem plantData={plantJSON} />
+    </>
+  );
 };
 
 export default Search;
--- src/containers/Search/SearchItems.js
+++ src/containers/Search/SearchItems.js
@@ -1,17 +1,26 @@
 /* React Imports */
 import React from "react";
-import './styles/SearchItem.css'
+import "./styles/SearchItem.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
-const SearchItem = ({}) => {
-  return sprouts.map(() => (
-
-
-    <div>
+const SearchItem = ({ plantData }) => {
+  const titleCase = (title) => title.charAt(0).toUpperCase() + title.slice(1);
+  return plantData.map((plant) => (
+    <div className="plant-search-item" key={plant["PLANT_ID"]}>
+      <img
+        src={plant["PLANT_IMG_URL"]}
+        className="plant-search-img"
+        alt="plant-search-img"
+      />
+      <div className="plant-search-name-div">
+        <strong>
+          <p className="plant-search-name">
+            {titleCase(plant["PLANT_COMMON_NAME"])}
+          </p>
+          </strong>
+          <p className="is-native" id={"plant" + plant["PLANT_ID"]}>Is Native</p>
+      </div>
     </div>
-    
-
   ));
 };
 
