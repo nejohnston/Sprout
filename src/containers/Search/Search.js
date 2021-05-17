338fd0aab24725fc6c421ea3066da57300dceb07
Sally Poon <sallypoon@hotmail.com>
2021-05-17 11:26:06 -0700

    Add header and SearchBar component files

--- src/containers/Search/Search.js
+++ src/containers/Search/Search.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import SearchBar from './SearchBar';
 
 const Search = () => {
   return (
-  <div>
-    <p>Hello World</p>
-  </div>);
+    <div id="container">
+      <div id="profile-header">
+        <h1 id="search-h1">Search</h1>
+      </div>
+
+      <hr/>
+
+      <SearchBar/>
+
+
+    </div>
+    );
+
 };
 
 export default Search;
---
+++ src/containers/Search/SearchBar.js
@@ -0,0 +1,14 @@
+/* React Imports */
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+const SearchBar = () => {
+  return(
+      <div>
+          hello world
+      </div>
+  );
+};
+
+export default SearchBar;
