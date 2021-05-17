27456a26808336dad365d084ad338624bb9219be
Sally Poon <sallypoon@hotmail.com>
2021-05-17 14:51:47 -0700

    Complete Search functoinality with Search Bar

--- src/containers/Search/Search.js
+++ src/containers/Search/Search.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import { React, useState } from 'react';
 import SearchBar from './SearchBar';
 
+/* Data import */
+import plantData from './plants.json';
+
 const Search = () => {
+
+  let plantDataStuff = [];
+  plantDataStuff.push(...plantData);
+
+  const [input, setInput] = useState('')
+  const [plantListDefault, setPlantListDefault] = useState(plantDataStuff);
+  const [plantList, setPlantList] = useState(plantDataStuff);
+
+  // console.log(plantListDefault)
+
+  const updateInput = input => {
+    const filterList = plantListDefault.filter(plant => {
+      return plant.PLANT_COMMON_NAME.toLowerCase().includes(input.toLowerCase())
+    })
+
+    setInput(input);
+    setPlantList(filterList);
+
+    console.log(filterList)
+  }
+
   return (
     <div id="container">
       <div id="profile-header">
@@ -10,7 +34,7 @@ const Search = () => {
 
       <hr/>
 
-      <SearchBar/>
+      <SearchBar input_keyword={input} updateInputKeyword={updateInput}/>
 
 
     </div>
--- src/containers/Search/SearchBar.js
+++ src/containers/Search/SearchBar.js
@@ -6,9 +6,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Form from "react-bootstrap/Form"
 import './styles/SearchBar.css'
 
-
-
-
-const SearchBar = () => <Form.Control type="text" placeholder="Search Plants ..."/>;
+/**
+ * Return the Search Bar component
+ * @returns Search Bar component
+ */
+const SearchBar = ({input_keyword, updateInputKeyword}) => 
+    <Form.Control 
+    type="text" 
+    placeholder="Search Plants ..."
+    value={input_keyword}
+    onChange= {event => updateInputKeyword(event.target.value)}/>; // event.target is the input node/ event.target.value is value attribute of the node
 
 export default SearchBar;
