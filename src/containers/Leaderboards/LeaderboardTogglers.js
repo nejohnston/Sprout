553823405d0ec13a79a078df78f1f0b0f80d6ef5
Susan Li <susansemail195@gmail.com>
2021-05-18 12:50:07 -0700

    Fix some errors ie missing props

--- src/containers/Leaderboards/LeaderboardTogglers.js
+++ src/containers/Leaderboards/LeaderboardTogglers.js
@@ -5,12 +5,13 @@ const LeaderboardTogglers = ({ teams }) => {
   return (
     <>
       <div id="leaderboard-togglers-container">
-        <div
-          className="team-toggler-container"
-          className="leaderboard-team-lower"
-        >
+        <div className="team-toggler-container leaderboard-team-lower">
           <div className="leaderboard-team-bg">
-            <img src={teams[1]["img_url"]} className="leaderboard-team-image" />
+            <img
+              src={teams[1]["img_url"]}
+              className="leaderboard-team-image"
+              alt=""
+            />
           </div>
           <div className="leaderboard-togglers-points">
             <p>{teams[1]["team_points"]}</p>
@@ -20,7 +21,11 @@ const LeaderboardTogglers = ({ teams }) => {
         </div>
         <div className="team-toggler-container">
           <div className="leaderboard-team-bg" id="leaderboard-first">
-            <img src={teams[0]["img_url"]} className="leaderboard-team-image" />
+            <img
+              src={teams[0]["img_url"]}
+              className="leaderboard-team-image"
+              alt=""
+            />
           </div>
           <div className="leaderboard-togglers-points-first">
             <p>{teams[0]["team_points"]}</p>
@@ -28,12 +33,13 @@ const LeaderboardTogglers = ({ teams }) => {
           </div>
           <p className="leaderboard-names">{teams[0]["team_name"]}</p>
         </div>
-        <div
-          className="team-toggler-container"
-          className="leaderboard-team-lower"
-        >
+        <div className="team-toggler-container leaderboard-team-lower">
           <div className="leaderboard-team-bg">
-            <img src={teams[2]["img_url"]} className="leaderboard-team-image" />
+            <img
+              src={teams[2]["img_url"]}
+              className="leaderboard-team-image"
+              alt=""
+            />
           </div>
           <div className="leaderboard-togglers-points">
             <p>{teams[2]["team_points"]}</p>
