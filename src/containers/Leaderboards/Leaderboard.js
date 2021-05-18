c3f42409b361eddba5bc80144458c57b8773ff1a
Sally Poon <sallypoon@hotmail.com>
2021-05-18 11:28:51 -0700

    Add top five background

--- src/containers/Leaderboards/Leaderboard.js
+++ src/containers/Leaderboards/Leaderboard.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import UnderConstruction from '../../components/Layout/UnderConstruction';
+
 import './styles/Leaderboard.css';
+import TopFive from './TopFive';
+
+import champions from './champions1.json';
 
 const Leaderboard = () => {
+
+  let championsJSON = [];
+  championsJSON.push(...champions);
+
   return (
     <div id="container">
     <div id="leaderboard-accent-bg"></div>
     <div id="profile-header">
     <h1 id="profile-h1">Leaderboard</h1>
-  </div>
+    </div>
 
-  <hr/>
+    <hr/>
 
+    <div id="top-five-container">
+      <TopFive topfive={championsJSON}/>
+
+      <div id="about-us-nav-block"></div>
+    </div>
 
 
   </div>
---
+++ src/containers/Leaderboards/TopFive.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const TopFive = ({ topfive }) => {
+    return topfive.map( player => {
+        <div>
+            <p>hello</p>
+        </div>
+    }
+    )
+}
+
+export default TopFive;
