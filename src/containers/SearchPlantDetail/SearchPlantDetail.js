932b3321a424d3dd95711e0778356616aadb5063
Sally Poon <sallypoon@hotmail.com>
2021-05-17 17:32:30 -0700

    Include back button, edit image cropping

--- src/containers/SearchPlantDetail/SearchPlantDetail.js
+++ src/containers/SearchPlantDetail/SearchPlantDetail.js
@@ -1,24 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import plantData from './plants.json';
 import DetailCards from './DetailCards'
 import AddPlantModal from '../../components/Modals/AddPlantModal';
+import backbutton from './images/back_button.svg';
 
 const SearchPlantDetail = () => {
   let plantJSON = [];
   plantJSON.push(...plantData);
 
+  /**
+   * Return the plant's first common name capitalized.
+   * @param {*} plant - a plant object
+   * @returns - a string, the plant's first common name capitalized.
+   */
   const firstCommonName = (plant) => {
-    return plant.PLANT_COMMON_NAME.split(',')[0];
+    let firstName = plant.PLANT_COMMON_NAME.split(',')[0];
+    return firstName.charAt(0).toUpperCase() + firstName.slice(1);
   }
 
   return (
   <div id="container">
-    <div id="profile-header">
+    <div id="header_backarrow_container">
+    <Link to="/search">
+          <img src={backbutton} className="back-button" alt="" />
+        </Link>
       <h1 id="profile-h1">{firstCommonName(plantJSON[2])}</h1>
       <AddPlantModal type={plantJSON[2]['PLANT_TYPE']} family={plantJSON[2]['PLANT_FAMILY_NAME']}/>
     </div>
 
     <hr />
+
+    <div id="search-details-img-container">
+    <img id="search-details-img" src={plantJSON[2]['PLANT_IMG_URL']}></img>
+    </div>
+
     <DetailCards plantInfo={plantJSON[2]}/>
   </div>);
 };
