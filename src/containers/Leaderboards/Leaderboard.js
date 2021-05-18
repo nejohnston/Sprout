8f6df58508082b421f496f1dfe928da50a8dea0e
Sally Poon <sallypoon@hotmail.com>
2021-05-18 10:06:44 -0700

    Background set up

--- src/containers/Leaderboards/Leaderboard.js
+++ src/containers/Leaderboards/Leaderboard.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import UnderConstruction from '../../components/Layout/UnderConstruction';
+import './styles/Leaderboard.css';
 
 const Leaderboard = () => {
   return (
-  <div>
-    <UnderConstruction />
-  </div>);
+    <div id="container">
+    <div id="leaderboard-accent-bg"></div>
+    <div id="profile-header">
+    <h1 id="profile-h1">Leaderboard</h1>
+  </div>
+
+  <hr/>
+
+
+
+  </div>
+  );
 };
 
 export default Leaderboard;
