e9ae11ee05b882fc4dcade2610dcf2b51ad5e75c
Susan Li <susansemail195@gmail.com>
2021-05-19 10:29:33 -0700

    Add documentation for Plant Profile Yop Options component

--- src/containers/PlantProfile/PlantProfileTopOptions.js
+++ src/containers/PlantProfile/PlantProfileTopOptions.js
@@ -1,9 +1,31 @@
+// =====================================
+//               IMPORTS
+// =====================================
+
+// React
 import React from "react";
+
+// Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
+
+// Styles
 import "./styles/PlantProfileTopOptions.css";
+
+// =====================================
+//          REACT COMPONENT
+// =====================================
 import DigUp from "./DigUp";
 import WaterPlant from "./WaterPlant";
 import PlantProfilePicture from "./PlantProfilePicture";
+
+/**
+ * Return the parent container object for the Plant Profile's top components
+ * 
+ * Pass the image url prop down into the child Plant Profile Picture component
+ * 
+ * @param {String} image_url - the url where the image of the plant is stored
+ * @returns the parent container object for the Plant Profile's top components
+ */
 
 const PlantProfileTopOptions = ({ image_url }) => {
   return (
