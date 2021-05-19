30931d176c9a711a5b7c0a1e5175e0ee79ac10e7
Susan Li <susansemail195@gmail.com>
2021-05-19 11:11:52 -0700

    Add documentation for Sprout Gallery component

--- src/containers/Profile/SproutGallery.js
+++ src/containers/Profile/SproutGallery.js
@@ -1,9 +1,29 @@
+// =====================================
+//               IMPORTS
+// =====================================
+
+// React
 import React from "react";
-import SproutItem from "./SproutItem";
+
+// Bootstrap
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import "./styles/SproutGallery.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+// Styles
+import "./styles/SproutGallery.css";
+
+// =====================================
+//          REACT COMPONENT
+// =====================================
+
+import SproutItem from "./SproutItem";
+
+/**
+ * Return the parent container of the user's sprouts (Sprout Items)
+ * @param {Array} sprouts - an array of objects containing the user's plant (sprout) information
+ * @returns the parent container of the user's sprouts (Sprout Items)
+ */
 
 const SproutGallery = ({ sprouts }) => {
   return (
