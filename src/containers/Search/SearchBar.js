5cd4a54f04cebf37929a6e0278a774a53d1358c1
Sally Poon <sallypoon@hotmail.com>
2021-05-19 11:23:57 -0700

    Write documentation

--- src/containers/Search/SearchBar.js
+++ src/containers/Search/SearchBar.js
@@ -1,20 +1,32 @@
-/* React Imports */
+// ====================================
+//            	IMPORT
+// ====================================
+
+// React
 import React from "react";
 
-/* Styling Imports */
+// Styles
 import "bootstrap/dist/css/bootstrap.min.css";
 import Form from "react-bootstrap/Form"
 import './styles/SearchBar.css'
 
+
+// ====================================
+//           REACT COMPONENT
+// ====================================
+
 /**
  * Return the Search Bar component
+ * @param {} inputKeyword - the currently typed input
+ * @param {} updateInputKeyword - function to update the filtered list and current inputKeyword
  * @returns Search Bar component
  */
-const SearchBar = ({input_keyword, updateInputKeyword}) => 
+const SearchBar = ({inputKeyword, updateInputKeyword}) => 
+
     <Form.Control 
     type="text" 
     placeholder="Search Plants ..."
-    value={input_keyword}
+    value={inputKeyword}
     onChange= {event => updateInputKeyword(event.target.value)}/>; // event.target is the input node/ event.target.value is value attribute of the node
 
 export default SearchBar;
--- src/containers/Search/SearchPage.js
+++ src/containers/Search/SearchPage.js
@@ -16,26 +16,45 @@ import SearchBar from "./SearchBar";
 // Data (Temp)
 import searchPlantData from "./plants.json";
 
-const Search = () => {
+
+
+// ====================================
+//           REACT COMPONENT
+// ====================================
+
+/**
+ * Return components of the Search Page.
+ * @returns - components of the Search Page.
+ */
+const SearchPage = () => {
 
   // Push data to be used as prop
   
   let plantJSON = [];
   plantJSON.push(...searchPlantData);
 
+  // State to track the search input changes as user types. Default empty string
   const [input, setInput] = useState('')
+
+  // State to track the default plant list. Default imported plant database
   const [plantListDefault, setPlantListDefault] = useState(plantJSON);
+
+  // State to track the filtered plant list. Default imported plant database
   const [plantList, setPlantList] = useState(plantJSON);
 
+
+  // Function to update the input in the search bar and the filtered plant list based on current input
   const updateInput = input => {
-    const filterList = plantListDefault.filter(plant => {
-      return plant.PLANT_COMMON_NAME.toLowerCase().includes(input.toLowerCase())
+
+    const filterList = plantListDefault.filter(plant => {   // filter through the default plant list
+      return plant.PLANT_COMMON_NAME.toLowerCase().includes(input.toLowerCase())  // return only plants w/ common name including input
     })
 
-    setInput(input);
-    setPlantList(filterList);
+    setInput(input);            // make sure input is updated in search bar to current input string
+    setPlantList(filterList);   // update the current plant list displayed to the new filtered list
   }
 
+  
   return (
     <div id="container">
       <div id="profile-header">
@@ -52,4 +71,4 @@ const Search = () => {
   );
 };
 
-export default Search;
+export default SearchPage;
