71f10d530f9fb76da2c075587c2c46554ab7b5ea
Susan Li <susansemail195@gmail.com>
2021-05-19 10:41:52 -0700

    Add documentation for Plant Profile Picture component

--- src/containers/PlantProfile/PlantProfilePicture.js
+++ src/containers/PlantProfile/PlantProfilePicture.js
@@ -1,7 +1,27 @@
+// =====================================
+//               IMPORTS
+// =====================================
+
+// React
 import React, { useState } from "react";
-import FsLightbox from "fslightbox-react"; // from https://fslightbox.com/react/documentation
+
+// FS Lightbox - from https://fslightbox.com/react/documentation
+import FsLightbox from "fslightbox-react"; 
+
+// Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
+
+// Styles
 import "./styles/PlantProfilePicture.css";
+
+/**
+ * Return the Plant Profile component
+ * 
+ * The image will lightbox, due to the incorporation of FS lightbox
+ * 
+ * @param {String} image_url - string with the url of the image to be rendered
+ * @returns the Plant Profile component
+ */
 
 const PlantProfilePicture = ({ image_url }) => {
   const [toggler, setToggler] = useState(false);
