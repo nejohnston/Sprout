54f5f292bac0aee9ec398e70c0c918a7c997d596
Sally Poon <sallypoon@hotmail.com>
2021-05-19 10:24:25 -0700

    Use bracket notation

--- src/containers/Leaderboards/TopFive.js
+++ src/containers/Leaderboards/TopFive.js
@@ -21,11 +21,11 @@ import './styles/TopFive.css';
 const TopFive = ({ topfive }) => {
     return topfive.map( player => (
         <>
-        <div key={player.user_id} className="champion-container">
-            <img className="champion-img shadow-sm" src={player.user_img} alt={player.username}/>
+        <div key={player["user_id"]} className="champion-container">
+            <img className="champion-img shadow-sm" src={player["user_img"]} alt={player["username"]}/>
             
-            <p className="champion-username"><strong>{player.username}</strong></p>
-            <p className="champion-points">{player.points}</p>
+            <p className="champion-username"><strong>{player["username"]}</strong></p>
+            <p className="champion-points">{player["points"]}</p>
 
         </div>
         <hr/>
