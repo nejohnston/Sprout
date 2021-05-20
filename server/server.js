752873be0d93649d9fef308e7b317c19b29339f0
Nicholas Johnston <nicholas.johnston94@gmail.com>
2021-05-19 22:59:10 -0700

    added documentation to server.js

--- server/server.js
+++ server/server.js
@@ -15,24 +15,34 @@ let app = express();
 // GET USER DATA
 /**
  * @params username, password
- * Return a user's data.
- * @returns - components of Profile Page.
+ * return a user's data.
+ * @returns - response from database.
  */
 app.get('/login/:username/:password', async (req, res) => {
-  let users = await getUsers(req.params.username, req.params.password);
-  res.json(users)
+  let user = await getUsers(req.params.username, req.params.password);
+  res.json(user)
 });
 
+// GET USER SPROUTS
+/**
+ * @params userId
+ * get a user's sprouts.
+ * @returns - sprouts of user.
+ */
 app.get('/sprouts/:userId', async (req, res) => {
   console.log(req.params.userId)
   let userSprouts = await getUserSprouts(req.params.userId);
-  
   res.json(userSprouts)
 });
 
+// POST NEW USER SPROUT
+/**
+ * @params sprout json object
+ * create new user sprout.
+ * @returns - success or fail message.
+ */
 app.post('/profile/', async (req, res) => {
   let addSprout = await createSprout(req.body)
-  // res.json(addSprout)
   console.log(req.body.wateringInterval)
 })
 
