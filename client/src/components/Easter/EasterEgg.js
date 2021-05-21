a3d9943ce174e9bcb1ac8196f235f307d9c6327f
Nicholas Johnston <nicholas.johnston94@gmail.com>
2021-05-20 22:04:21 -0700

    added corgi alt image, styled animationn

--- client/src/components/Easter/EasterEgg.js
+++ client/src/components/Easter/EasterEgg.js
@@ -1,11 +1,15 @@
 import React from 'react';
-
+import GifPlayer from 'react-gif-player'
+import corgiStill from '../../config/assets/images/Corgiswimflipflip.jpg'
+import corgi from '../../config/assets/images/corgiswim.gif'
 import './Easter.css';
 
 const EasterEgg = () => {
   return (
     <div id='guichonRiver'>
-      this is an easter egg
+      <div class="flier">
+        <img src={corgi} alt={corgiStill} />
+      </div>
     </div>
   )
 };
